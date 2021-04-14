7cc3a1a117325e1862ff25ab7a279d7a32addcaf
dboren <danielboren@att.net>
2021-04-14 15:53:40 -0500

    removed duc.replace from logout.js and added const response. debuging with askBCS

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,17 +1,18 @@
 
 
 const logout = async () => {
-    await fetch('/api/user/logout', {
+    const response = await fetch('/api/user/logout', {
        method: 'POST',
        headers: { 'Content-Type': 'application/json' },
      });
      if (response.ok) {
        console.log('logout succesfful')
        //res.redirect('/');
-       document.location.replace('/');
-     // } else {
-     //   alert(response.statusText);
-     // }
+      //  document.location.replace('/login');
+     } else {
+       alert(response.statusText);
+       console.log("status-text: ", response.statusText);
+     }
    };
-  };
-   document.querySelector('#logout').addEventListener('submit', logout);
+
+   document.querySelector('#logout').addEventListener('click', logout);
