c44b3f9bd7d78afcd4fe8376b7b227d9f2a73de0
Kannathal Vairavan <kannamanickam@gmail.com>
2021-04-13 20:55:27 -0500

    Added dashboard route in homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,38 +18,78 @@ router.get('/', async (req, res) => {
 }
 });
 
-//  populates all user related hobbies on the side of the screeen
-router.get('/hobby/:id', async (req, res) => {
-    console.log("hobby get route hit")
-    // req.session.user_id=1
-    try {
-      const hobbyData = await User.findByPk(req.params.id, {
+    // //  populates all user related hobbies on the side of the screeen
+    // router.get('/hobby/:id', async (req, res) => {
+    //     console.log("hobby get route hit")
+    //     // req.session.user_id=1
+    //     try {
+    //       const hobbyData = await User.findByPk(req.params.id, {
 
-        include:[
-             {
-                model: Hobby,
-                attributes: ["id", "name", "user_id"]
-             }
-        ]
-      })
-      console.log("hobbyData 40", hobbyData);
-    const hobbies =  hobby.get({ plain: true }); 
-    // hobbyData.map((hobby) =>
-    // const hobbies = hobbyData.get({ plain: true });
-    console.log('hobbies 43', JSON.stringify(hobbies.dataValues))
-    // console.log("hobby data[0]", hobbyData[0].hobbies[0])
-//     const arrayHobbies={
-//         hobbies: hobbyData.hobbies
-//    }
-//    console.log(arrayHobbies)
-//    res.render('dashboard', arrayHobbies);
-    res.render('dashboard', { hobbies, loggedIn: req.session.loggedIn});
-      }catch (err) {
-      res.json(err);
-    }
-  }
+    //         include:[
+    //             {
+    //                 model: Hobby,
+    //                 attributes: ["id", "name", "user_id"]
+    //             }
+    //         ]
+    //       })
+    //       console.log("hobbyData 40", hobbyData);
+    //     const hobbies =  hobby.get({ plain: true }); 
+    //     // hobbyData.map((hobby) =>
+    //     // const hobbies = hobbyData.get({ plain: true });
+    //     console.log('hobbies 43', JSON.stringify(hobbies.dataValues))
+    //     // console.log("hobby data[0]", hobbyData[0].hobbies[0])
+    // //     const arrayHobbies={
+    // //         hobbies: hobbyData.hobbies
+    // //    }
+    // //    console.log(arrayHobbies)
+    // //    res.render('dashboard', arrayHobbies);
+    //     res.render('dashboard', { hobbies, loggedIn: req.session.loggedIn});
+    //       }catch (err) {
+    //       res.json(err);
+    //     }
+    //   }
+      
+    //   );
+// , withAuth
+// , loggedIn: req.session.loggedIn
+
+
+router.get('/dashboard', async (req, res) => {
+  try {
+      
+     
+          res.render('dashboard');
   
-  );
+  
+    }catch (err) {
+      console.log(err); 
+    res.status(500).json(err);
+  }
+});
+
+
+  router.get('/hobby/:id', async (req, res) => {
+    try {
+        const dbHobbyData = await User.findByPk(req.params.id,{
+          include:[
+            {
+              model:Hobby,
+              attributes:['id', 'name', 'user_id' ]
+            }
+        ],
+        });
+        const hobbyList = dbHobbyData.get({ plain: true });
+        console.log(hobbyList);
+            res.render('dashboard', { hobbyList });
+    
+    
+      }catch (err) {
+        console.log(err); 
+      res.status(500).json(err);
+    }
+  });
+
+
 
 
 router.get('/Notes/:id', withAuth, async (req, res) => {
