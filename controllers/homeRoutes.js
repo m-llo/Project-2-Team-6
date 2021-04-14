d732b5eda257e0941b2e68a60ec42c495f3f8150
Kannathal Vairavan <kannamanickam@gmail.com>
2021-04-13 20:16:00 -0500

    moved get hobby route to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const { Hobby, User, Videos, Notes } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   // if (req.session.logged_in) {
@@ -16,5 +18,59 @@ router.get('/', async (req, res) => {
 }
 });
 
+//  populates all user related hobbies on the side of the screeen
+router.get('/hobby/:id', async (req, res) => {
+    console.log("hobby get route hit")
+    // req.session.user_id=1
+    try {
+      const hobbyData = await User.findByPk(req.params.id, {
+
+        include:[
+             {
+                model: Hobby,
+                attributes: ["id", "name", "user_id"]
+             }
+        ]
+      })
+      console.log("hobbyData 40", hobbyData);
+    const hobbies =  hobby.get({ plain: true }); 
+    // hobbyData.map((hobby) =>
+    // const hobbies = hobbyData.get({ plain: true });
+    console.log('hobbies 43', JSON.stringify(hobbies.dataValues))
+    // console.log("hobby data[0]", hobbyData[0].hobbies[0])
+//     const arrayHobbies={
+//         hobbies: hobbyData.hobbies
+//    }
+//    console.log(arrayHobbies)
+//    res.render('dashboard', arrayHobbies);
+    res.render('dashboard', { hobbies, loggedIn: req.session.loggedIn});
+      }catch (err) {
+      res.json(err);
+    }
+  }
+  
+  );
+
+
+router.get('/Notes/:id', withAuth, async (req, res) => {
+  try {
+      const dbVideoData = await Videos.findByPk(req.params.id,{
+        include:[
+          {
+            model:Notes,
+            attributes:['id', 'title', 'text' ]
+          }
+      ],
+      });
+      const NotesList = dbVideoData.get({ plain: true });
+      console.log(NotesList);
+          res.render('videoView', { NotesList, loggedIn: req.session.loggedIn });
+  
+  
+    }catch (err) {
+      console.log(err); 
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
