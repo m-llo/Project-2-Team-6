da2b86ebd147309ad2b9fed501e618e76429a390
m-llo <markalloyd21@gmail.com>
2021-04-13 19:10:37 -0500

    more  test of hobby routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,17 +2,20 @@ const router = require('express').Router();
 
 
 router.get('/', async (req, res) => {
-  if (req.session.logged_in) {
+  // if (req.session.logged_in) {
     // alert('Welcome back, you are currently signed in. Click OK to proceed to your dashboard.');    
-    res.redirect('/api/dashboard');
-        return;
-      }else{
+    // res.redirect('/api/dashboard');
+    //     return;
+      // }
+      // else{
   try {
     res.render('login');
   } catch (err) {
-    res.status(500);
+    res.json(err);
     console.log(err);
-  }}
+  }
+// }
 });
+
 
 module.exports = router;
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,16 @@
+
+
 const logout = async () => {
-    await fetch('api/user/logout', {
+    await fetch('/api/user/logout', {
        method: 'POST',
        headers: { 'Content-Type': 'application/json' },
      });
      if (response.ok) {
-       console.log('logout succesfful')
-       document.location.replace('/login');
-     // } else {
-     //   alert(response.statusText);
-     // }
+  //      console.log('logout succesfful')
+      document.location.replace('/');
+  //    // } else {
+  //    //   alert(response.statusText);
+  //    // }
    };
   };
    document.querySelector('#logout').addEventListener('submit', logout);
