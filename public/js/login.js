d227c4b94a1d74cc5b3df165a53b15661e43aa30
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-04-16 16:39:15 -0500

    nodemailer successful response in email

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+//const { response } = require("express");
 
 //user login
 const loginFormHandler = async (event) => {
@@ -30,7 +30,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-signup').value.trim();
   
     if (username && email && password) {
-      console.log('this is the if statment');
+      console.log('this is the if statment in sign');
       const response = await fetch('/api/user/', {
         method: 'POST',
         body: JSON.stringify({ username, email, password }),
